c289ec87fdc517ab20e7192e7c765aac210d3637
Keith Wallace <keith.t.wallace@gmail.com>
2018-09-20 13:22:13 -0400

    Install and init React/ReactDOM

---
+++ src/App.react.js
@@ -0,0 +1,17 @@
+import React from 'react'
+import ReactDOM from 'react-dom'
+
+const App = () => {
+  return (
+    <div>
+      <p>React here!</p>
+    </div>
+  )
+}
+
+export default App
+
+ReactDOM.render(
+  <App />,
+  document.getElementById('app')
+)
